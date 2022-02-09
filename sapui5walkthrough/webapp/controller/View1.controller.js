2e0b10127936e4fad7f5d63503d8375650c94c55
Curtis Kettler <curtiskettler@Curtiss-MBP.lan>
2022-02-09 11:49:34 -0600

    step 6 complete

--- sapui5walkthrough/webapp/controller/View1.controller.js
+++ sapui5walkthrough/webapp/controller/View1.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("mindset.curtiskettler.sapui5walkthrough.sapui5walkthrough.controller.View1", {
             onInit: function () {
-
+                var oData = {
+                    recipient : {
+                        name: "World"
+                    }
+                };
+                var oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+            },
+            onShowHello : function () {
+                MessageToast.show("Hello World!");
             }
         });
     });
