70b1702c72f397a0bb73f612b2fb686423b95ac5
Curtis Kettler <curtiskettler@Curtiss-MacBook-Pro.local>
2022-02-10 09:52:02 -0600

    Step 8 complete

--- sapui5walkthrough/webapp/controller/View1.controller.js
+++ sapui5walkthrough/webapp/controller/View1.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast, JSONModel) {
+    function (Controller, MessageToast, JSONModel, ResourceModel) {
         "use strict";
 
         return Controller.extend("mindset.curtiskettler.sapui5walkthrough.sapui5walkthrough.controller.View1", {
@@ -20,7 +21,11 @@ sap.ui.define([
                 this.getView().setModel(oModel);
             },
             onShowHello : function () {
-                MessageToast.show("Hello World!");
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+                MessageToast.show(sMsg);
             }
         });
     });
